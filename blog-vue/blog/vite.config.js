199fd6206e79425abef1f36ea78175bf6767931b
caiguoyu <caiguoyu@vip.qq.com>
2026-05-20 14:27:34 +0800

    chore(blog): 补充前台构建配置

--- blog-vue/blog/vite.config.js
+++ blog-vue/blog/vite.config.js
@@ -2,11 +2,20 @@ import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue2";
 
+const rootVue = fileURLToPath(
+  new URL("./node_modules/vue/dist/vue.runtime.esm.js", import.meta.url)
+);
+
 export default defineConfig({
   plugins: [vue()],
   resolve: {
+    dedupe: ["vue"],
     extensions: [".mjs", ".js", ".ts", ".jsx", ".tsx", ".json", ".vue"],
     alias: [
+      {
+        find: /^vue$/,
+        replacement: rootVue
+      },
       {
         find: /^vue-social-share$/,
         replacement: fileURLToPath(
